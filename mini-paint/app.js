875785f46db526afbb4cf6d7ea599a668b937ae5
Anton Bredl <antonbr13@gmail.com>
2016-02-11 14:57:43 -0700

    finished the paint project

--- mini-paint/app.js
+++ mini-paint/app.js
@@ -1,40 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	var color = "white";
-		//selector //listener //callback
-	$('.box').on("click", function(){
-		$(this).addClass(color); // function body
-	})
 
-	$(".box").on('dblclick', function(){
-		$(this).removeClass(color);
-	})
+	var colors = 'white red green blue yellow';
 
-	$('#reset').on('click', function(){
-		$('.box').removeClass('red blue green yellow white');
-	})
+	var color = 'white';
 
-	$('#red').on('click', function(){
-		color = 'red';
-	})
 
-	$('#blue').on('click', function(){
-		color = 'blue';
-	})
+	$('.box').on('click', function() {
+	  $(this).addClass(color);
+	});
 
-	$('#green').on('click', function(){
-		color = 'green';
-	})
+	$('.box').on('dblclick', function() {
+	  $(this).removeClass(colors);
+	});
 
-	$('#yellow').on('click', function(){
-		color = 'yellow';
-	})
-
-	$('#white').on('click', function(){
-		color = 'white';
-	})
+	$('#reset').on('click', function() {
+	  $('.box').removeClass(colors);
+	});
 
 
 
 
-})
+	$('#red').on('click', function() {
+	  color = 'red';
+	});
+
+	$('#blue').on('click', function() {
+	  color = 'blue';
+	});
+
+	$('#green').on('click', function() {
+	  color = 'green';
+	});
+
+	$('#yellow').on('click', function() {
+	  color = 'yellow';
+	});
+
+	$('#white').on('click', function() {
+	  color = 'white';
+	});
+
+
+});
